4420745309ebe5f62ff8ea4a0f6d8b4e2e4b45c5
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-02-19 19:23:52 +0300

    Updates

---
+++ precache-manifest.9a92f92c10a372572fe547528e38ef25.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192d9a3fefda023949c302de33012b2c",
+    "url": "/NoLogoStydio-test-task/index.html"
+  },
+  {
+    "revision": "0498adfbcc18ba54e2f7",
+    "url": "/NoLogoStydio-test-task/static/css/main.154182d4.chunk.css"
+  },
+  {
+    "revision": "33913147b05ecbcb74cd",
+    "url": "/NoLogoStydio-test-task/static/js/2.22b145b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/NoLogoStydio-test-task/static/js/2.22b145b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0498adfbcc18ba54e2f7",
+    "url": "/NoLogoStydio-test-task/static/js/main.5aaacfbd.chunk.js"
+  },
+  {
+    "revision": "438bfcf0efefcba7d408",
+    "url": "/NoLogoStydio-test-task/static/js/runtime-main.26db0908.js"
+  },
+  {
+    "revision": "4984c99734c50b4dd921499bb8c794f3",
+    "url": "/NoLogoStydio-test-task/static/media/Chevron.4984c997.svg"
+  },
+  {
+    "revision": "1c840b7217b615618b67520a7c9083e2",
+    "url": "/NoLogoStydio-test-task/static/media/Chevron_white.1c840b72.svg"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/NoLogoStydio-test-task/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "98f67dd2b0d0e19f60b2d077695e8b9e",
+    "url": "/NoLogoStydio-test-task/static/media/icon-01-menuFirstLevel.98f67dd2.svg"
+  },
+  {
+    "revision": "575bd781e90f74ab065d7657074f900e",
+    "url": "/NoLogoStydio-test-task/static/media/icon-01-menuSecondLevel__lemonade.575bd781.svg"
+  },
+  {
+    "revision": "940fa65ce0e3bd41cb3c8a5cf5ca9503",
+    "url": "/NoLogoStydio-test-task/static/media/icon-02-menuFirstLevel.940fa65c.svg"
+  },
+  {
+    "revision": "155064eb62c28bb1a4c35e8099ee5542",
+    "url": "/NoLogoStydio-test-task/static/media/icon-02-menuSecondLevel__gas.155064eb.svg"
+  },
+  {
+    "revision": "cccb366a6f46b88f62e65ea8c61ab1b2",
+    "url": "/NoLogoStydio-test-task/static/media/icon-03-menuFirstLevel.cccb366a.svg"
+  },
+  {
+    "revision": "62e1d92ee814962304ec56b6028c3170",
+    "url": "/NoLogoStydio-test-task/static/media/icon-03-menuSecondLevel__flask.62e1d92e.svg"
+  },
+  {
+    "revision": "e75c1da47f5189e26363c53d80d2ba6c",
+    "url": "/NoLogoStydio-test-task/static/media/icon-04-menuFirstLevel.e75c1da4.svg"
+  },
+  {
+    "revision": "13fdc061f0b0b37767ce0888032ff17c",
+    "url": "/NoLogoStydio-test-task/static/media/icon-05-menuFirstLevel.13fdc061.svg"
+  }
+]);
