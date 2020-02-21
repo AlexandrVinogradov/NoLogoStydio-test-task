243156a042ff6aeb5520ffe4ac34018c9baede62
AlexandrVinogradov <ale.grapes.webdev@gmail.com>
2020-02-21 14:23:18 +0300

    async width trouble

--- src/components/MenuFirstLevel/MenuFirstLevel.jsx
+++ src/components/MenuFirstLevel/MenuFirstLevel.jsx
@@ -6,10 +6,91 @@ import icon03 from './MenuFirstLevel__icons/icon-03-menuFirstLevel.svg'
 import icon04 from './MenuFirstLevel__icons/icon-04-menuFirstLevel.svg'
 import icon05 from './MenuFirstLevel__icons/icon-05-menuFirstLevel.svg'
 
-const MenuFirstLevel = () => {
-  return (
+class MenuFirstLevel extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.flatConteinerRef = React.createRef()
+    this.openConteinerRef = React.createRef()
+    this.tankConteinerRef = React.createRef()
+    this.termConteinerRef = React.createRef()
+    this.uniConteinerRef = React.createRef()
+
+    this.state = {
+      windowWidth: window.innerWidth,
+
+      flatConteinerWidth: 0,
+      openConteinerWidth: 0,
+      tankConteinerWidth: 0,
+      termConteinerWidth: 0,
+      uniConteinerWidth: 0,
+
+      navWidth: 0,
+      isHide: false,
+    }
+  }
+
+  navWidthCalculate = () => {
+    const {
+      flatConteinerWidth,
+      openConteinerWidth,
+      tankConteinerWidth,
+      termConteinerWidth,
+      uniConteinerWidth,
+    } = this.state
+
+    this.setState({
+      navWidth: flatConteinerWidth + openConteinerWidth + tankConteinerWidth + termConteinerWidth + uniConteinerWidth,
+    })
+  }
+
+  test = () => {
+    const { windowWidth, navWidth } = this.state
+    if (windowWidth < navWidth) {
+      this.setState({
+        isHide: true,
+      })
+    }
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('resize', this.handleResize)
+    // console.log(this.flatConteinerRef.current.offsetWidth)
+    // setTimeout(() => {
+    //   console.log(this.flatConteinerRef.current.offsetWidth)
+    //   console.log(this.flatConteinerRef)
+    // }, 1000)
+
+    this.setState(
+      {
+        flatConteinerWidth: this.flatConteinerRef.current.clientWidth,
+        openConteinerWidth: this.openConteinerRef.current.clientWidth,
+        tankConteinerWidth: this.tankConteinerRef.current.clientWidth,
+        termConteinerWidth: this.termConteinerRef.current.clientWidth,
+        uniConteinerWidth: this.uniConteinerRef.current.clientWidth,
+      },
+      () => {
+        this.navWidthCalculate()
+      }
+    )
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  handleResize = () => {
+    this.setState({
+      windowWidth: window.innerWidth,
+    })
+  }
+
+  render() {
+    // console.log(this.state.navWidth);
+    return (
       <ul className={s.menuFirstLevel}>
-        <li className={s.menuItem_flatRack}>
+
+        <li ref={this.flatConteinerRef} className={s.menuItem_flatRack}>
           <div>
             <img alt="flat Rack контецнеры" src={icon01} />
           </div>
@@ -18,7 +99,8 @@ const MenuFirstLevel = () => {
             <span>контейнеры</span>
           </div>
         </li>
-        <li className={s.menuItem_openTop}>
+
+        <li ref={this.openConteinerRef} className={s.menuItem_openTop}>
           <div>
             <img alt="Опен топ контецнеры" src={icon02} />
           </div>
@@ -27,7 +109,7 @@ const MenuFirstLevel = () => {
             <span>Контейнеры</span>
           </div>
         </li>
-        <li className={s.menuItem_tankConteiner}>
+        <li ref={this.tankConteinerRef} className={s.menuItem_tankConteiner}>
           <div>
             <img alt="Танк-топ контецнеры" src={icon03} />
           </div>
@@ -36,7 +118,7 @@ const MenuFirstLevel = () => {
             <span>Контейнеры</span>
           </div>
         </li>
-        <li className={s.menuItem_termConteiner}>
+        <li ref={this.termConteinerRef} className={s.menuItem_termConteiner}>
           <div>
             <img alt="Термо-контецнеры" src={icon04} />
           </div>
@@ -45,7 +127,7 @@ const MenuFirstLevel = () => {
             <span>Контейнеры</span>
           </div>
         </li>
-        <li className={s.menuItem_universalConteiner}>
+        <li ref={this.uniConteinerRef} className={s.menuItem_universalConteiner}>
           <div>
             <img alt="Универсальный контецнеры" src={icon05} />
           </div>
@@ -55,7 +137,8 @@ const MenuFirstLevel = () => {
           </div>
         </li>
       </ul>
-  )
+    )
+  }
 }
 
 export default MenuFirstLevel
